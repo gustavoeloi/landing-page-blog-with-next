f526580ff8a78f671933b7c273c3fc1679ceeae9
gustavooeloi <gustavooeloi@gmail.com>
2026-03-28 09:34:16 -0300

    refact customer-story-card

--- src/components/customer-story-card/customer-story-card.tsx
+++ src/components/customer-story-card/customer-story-card.tsx
@@ -1,34 +1,37 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 type CustomerStoryCardProps = {
-  text: string;
-  name: string;
-  subtitle: string;
-  avatarPath: string;
+  content: string;
+  author: {
+    name: string;
+    role: string;
+    avatar: string;
+  };
 };
 
 export const CustomerStoryCard = ({
-  text,
-  name,
-  subtitle,
-  avatarPath,
+  content,
+  author,
 }: CustomerStoryCardProps) => {
+  console.log(author);
   return (
     <div className="flex flex-col gap-6 md:gap-8 bg-gray-600 border border-gray-400 p-6 md:p-10 rounded-md">
-      <p className="italic text-gray-200 text-justify text-body-md">{text}</p>
+      <p className="italic text-gray-200 text-justify text-body-md">
+        {content}
+      </p>
 
       <div className="flex items-center gap-4">
         <Avatar>
           <AvatarImage
-            src={avatarPath}
+            src={author.avatar}
             alt="avatar"
             className="border border-blue-200"
           />
-          <AvatarFallback>{name}</AvatarFallback>
+          <AvatarFallback>{author.name}</AvatarFallback>
         </Avatar>
         <div>
-          <h4 className="text-gray-200 text-heading-xs">{name}</h4>
-          <p className="text-gray-300 text-body-sm">{subtitle}</p>
+          <h4 className="text-gray-200 text-heading-xs">{author.name}</h4>
+          <p className="text-gray-300 text-body-sm">{author.role}</p>
         </div>
       </div>
     </div>
--- src/components/customer-story-section/customer-story-section.tsx
+++ src/components/customer-story-section/customer-story-section.tsx
@@ -1,6 +1,27 @@
 import { CustomerStoryCard } from "../customer-story-card";
 
 export const CustomerStorySection = () => {
+  const customerStories = [
+    {
+      content:
+        "Criar minha loja com o site.set foi a melhor decisão para o meu negócio. A plataforma é super intuitiva, e consegui colocar meus produtos à venda em poucos minutos.",
+      author: {
+        name: "Annete Bones",
+        role: "CEO na Anne Corp",
+        avatar: "/avatar-1.png",
+      },
+    },
+    {
+      content:
+        "Transformar minha ideia em uma loja online foi fácil e rápido. Adorei as opções de personalização e a simplicidade para gerenciar os pedidos. Já vejo meus produtos alcançando mais pessoas!",
+      author: {
+        name: "Jacob Jones",
+        role: "CEO na JJ Corp",
+        avatar: "/avatar-2.jpg",
+      },
+    },
+  ];
+
   return (
     <section className="container py-32">
       <div className="flex flex-col items-center gap-12">
@@ -8,18 +29,12 @@ export const CustomerStorySection = () => {
           Quem utiliza, aprova!
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-6">
-          <CustomerStoryCard
-            text="Criar minha loja com o site.set foi a melhor decisão para o meu negócio. A plataforma é super intuitiva, e consegui colocar meus produtos à venda em poucos minutos. A"
-            name="Annette Bon es"
-            subtitle="CEO na Anne Corp"
-            avatarPath="/avatar-1.png"
-          />
-          <CustomerStoryCard
-            text="Transformar minha ideia em uma loja online foi fácil e rápido. Adorei as opções de personalização e a   para gerenciar os pedidos. Já vejo meus produtos alcançando mais pessoas!"
-            name="Jacob Jones"
-            subtitle="CEO na JJ Org"
-            avatarPath="/avatar-2.jpg"
-          />
+          {customerStories.map((customer) => (
+            <CustomerStoryCard
+              author={customer.author}
+              content={customer.content}
+            />
+          ))}
         </div>
       </div>
     </section>
