bc5600f8addd1f950e1e54e8229b960625e96360
gustavooeloi <gustavooeloi@gmail.com>
2026-04-02 10:17:29 -0300

    começando a migrar para o App Router

--- src/components/active-link/active.link.tsx
+++ src/components/active-link/active.link.tsx
@@ -1,21 +1,23 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import Link, { LinkProps } from "next/link";
-import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 
 type ActiveLink = {
   children: React.ReactNode;
 } & LinkProps;
 
 export const ActiveLink = ({ children, href, ...rest }: ActiveLink) => {
-  const router = useRouter();
+  const linkPath = (typeof href === "string" ? href : href.pathname) ?? "";
+  const pathName = usePathname();
 
   const isCurrentPath =
-    router.asPath === href ||
-    router.asPath === rest.as ||
-    router.asPath.startsWith(String(rest.as));
+    pathName === linkPath || pathName?.startsWith(`${linkPath}/`);
 
   return (
     <Link
+      {...rest}
       href={href}
       className={cn(
         "text-sm font-medium transition-colors hover:text-blue-200",
--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,14 +1,11 @@
-import Link from "next/link";
-import Image from "next/image";
+"use client";
 
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Logo } from "@/components/logo";
-import { useRouter } from "next/router";
 import { ActiveLink } from "../../active-link";
 
 export const Header = () => {
-  const router = useRouter();
-
   return (
     <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-backgroud/95 backdrop-blur supports-backdrop-filters:bg-background/60">
       <div className="container px-4 sm:px-6 lg:px-8">
